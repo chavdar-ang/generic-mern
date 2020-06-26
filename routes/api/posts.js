2ecc4127132d2fb557bca71061636f3c891a456b
Chavdar Angelov <chavdar84@gmail.com>
2020-06-26 17:54:46 +0300

    Crud functionality.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,34 +4,85 @@ const Post = require("../../models/Post");
 
 router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
-// @access  Public
+// @route   GET api/{model}
+// @desc    Index route
+// @access  --
 
-router.get("/", (req, res) => {
-  const cursor = db.collection('quotes').find()
-  console.log(cursor)
-  res.send("Posts route")
+router.get("/", async (req, res) => {
+  try {
+    const documents = await Post.find();
+    res.json(documents);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+  res.send(req.document);
+});
+
+// @route   GET api/{model}/{id}
+// @desc    Show route
+// @access  --
+
+router.get("/:id", getDocument, (req, res) => {
+  res.json(res.document);
 });
 
 // @route   POST api/users
 // @desc    Register route
 // @access  Public
 
-router.post("/test", async (req, res) => {
+router.post("/", async (req, res) => {
   console.log(req.body);
 
-  const post = new Post(req.body);
+  const document = new Post(req.body);
 
   try {
-    let data = await post.save();
-    res.status(201).json(data);
+    let newDocument = await document.save();
+    res.status(201).json(newDocument);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-console.log(router);
+// @route   PATCH api/{model}/{id}
+// @desc    Update route
+// @access  --
+router.patch("/:id", getDocument, async (req, res) => {
+  // add request values to the document object
+  Object.assign(res.document, req.body);
 
+  try {
+    const updatedDocument = await res.document.save();
+    res.json(updatedDocument);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// @route   DELETE api/{model}/{id}
+// @desc    Update route
+// @access  --
+router.delete("/:id", getDocument, async (req, res) => {
+  try {
+    await res.document.remove();
+    res.json({ message: "Document deleted." });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getDocument(req, res, next) {
+  let document;
+  try {
+    document = await Post.findById(req.params.id);
+    if (document == null) {
+      return res.status(404).json({ message: "Document not found." });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.document = document;
+  next();
+}
+console.log(router);
 
 module.exports = router;
