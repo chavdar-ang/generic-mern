213240c23af9932fa52242d743f3c180680ff677
Chavdar Angelov <chavdar84@gmail.com>
2020-10-15 17:50:41 +0300

    CORS quick fix.

--- src/App/app.js
+++ src/App/app.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 5000;
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => res.send("API is running"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
